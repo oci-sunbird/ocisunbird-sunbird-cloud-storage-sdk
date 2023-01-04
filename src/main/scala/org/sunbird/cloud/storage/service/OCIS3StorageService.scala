bc2acd95e4b593c85d54c65a17c57e3fbacf5bca
Deepak Devadathan <deepak.devadathan@oracle.com>
2023-01-04 05:57:20 +0000

    added log4j logging for tracing purpose..will be removed later
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- src/main/scala/org/sunbird/cloud/storage/service/OCIS3StorageService.scala
+++ src/main/scala/org/sunbird/cloud/storage/service/OCIS3StorageService.scala
@@ -2,17 +2,30 @@ package org.sunbird.cloud.storage.service
 
 import org.jclouds.ContextBuilder
 import org.jclouds.blobstore.BlobStoreContext
+import com.google.inject.Module
+import com.google.common.collect._
 import org.sunbird.cloud.storage.BaseStorageService
 import org.sunbird.cloud.storage.Model.Blob
 import org.sunbird.cloud.storage.factory.StorageConfig
 import org.jclouds.logging.slf4j.config.SLF4JLoggingModule
+// import ch.qos.logback.classic.Logger;
+// import ch.qos.logback.classic.LoggerContext;
+import org.jclouds.logging.slf4j.SLF4JLogger
+
 import java.io.File
 import java.util.Properties
 
 class OCIS3StorageService(config: StorageConfig) extends BaseStorageService {
 
     val overrides = new Properties()
-    Iterable<Module> modules = ImmutableSet.<Module> of(new SLF4JLoggingModule())
+    // Iterable<Module> modules = ImmutableSet.<Module> of(new SLF4JLoggingModule())
+    // var modules = ImmutableSet.<Module> of(new SLF4JLoggingModule())
+    // val modules: Set[Iterable]=Set(new SLF4JLoggingModule())
+    // val modules: Iterable[Module] = Set(new SLF4JLoggingModule().asInstanceOf[Module]).toIterable
+    val modules = ImmutableSet.of(new SLF4JLoggingModule().asInstanceOf[Module])
+        // val modules: Iterable[Module]  = ImmutableSet <Module> of(new SLF4JLoggingModule())
+
+    // println(ImmutableSet.of(new SLF4JLoggingModule().asInstanceOf[Module]).getClass)
 
     overrides.setProperty("jclouds.provider", "s3")
     overrides.setProperty("jclouds.endpoint", "https://apaccpt03.compat.objectstorage.us-ashburn-1.oraclecloud.com")
